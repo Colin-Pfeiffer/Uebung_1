b40b3d92825e74d6234d53c50939c7ce397cc3a7
Colin Pfeiffer <colin.pfeiffer@web.de>
2023-09-16 18:46:09 +0200

    optimised existing code

--- uebung_1.py
+++ uebung_1.py
@@ -1,12 +1,12 @@
 import os
-from random import randint
+import random
 import pygame
 
 def random_obstacle_position_x() -> int:
-    return randint(75, Settings.WINDOW.width - Settings.OBSTACLE_SIZE)
+    return random.randint(75, Settings.WINDOW.width - Settings.OBSTACLE_SIZE)
 
 def random_obstacle_position_y() -> int:
-    return randint(75, Settings.WINDOW.height - Settings.OBSTACLE_SIZE)
+    return random.randint(75, Settings.WINDOW.height - Settings.OBSTACLE_SIZE)
 
 class Settings:
     WINDOW = pygame.rect.Rect(0, 0, 960, 540)
@@ -15,50 +15,15 @@ class Settings:
     ASSET_PATH = os.path.join(FILE_PATH, "assets")
     IMAGE_PATH = os.path.join(FILE_PATH, ASSET_PATH, "images")
     OBSTACLE_SIZE = 50
+    OBSTACLE_NUM = 5
     OBJECT_SIZE = 30
-    OBJECT_SPAWNRATE = 250
+    OBJECT_SPAWNRATE = 200
 
-class Test1(pygame.sprite.Sprite):
-    def __init__(self) -> None:
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, image: str, scale_multiply_x: float = 1.0, scale_multiply_y: float = 1.0) -> None:
         super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "test.png")).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE, Settings.OBSTACLE_SIZE))
-        self.rect = self.image.get_rect()
-        self.rect.left = random_obstacle_position_x()
-        self.rect.bottom = random_obstacle_position_y()
-
-class Test2(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "test.png")).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE, Settings.OBSTACLE_SIZE))
-        self.rect = self.image.get_rect()
-        self.rect.left = random_obstacle_position_x()
-        self.rect.bottom = random_obstacle_position_y()
-
-class Test3(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "test.png")).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE, Settings.OBSTACLE_SIZE))
-        self.rect = self.image.get_rect()
-        self.rect.left = random_obstacle_position_x()
-        self.rect.bottom = random_obstacle_position_y()
-
-class Test4(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "test.png")).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE, Settings.OBSTACLE_SIZE))
-        self.rect = self.image.get_rect()
-        self.rect.left = random_obstacle_position_x()
-        self.rect.bottom = random_obstacle_position_y()
-
-class Test5(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "test.png")).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE, Settings.OBSTACLE_SIZE))
+        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, image)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (Settings.OBSTACLE_SIZE * scale_multiply_x, Settings.OBSTACLE_SIZE * scale_multiply_y))
         self.rect = self.image.get_rect()
         self.rect.left = random_obstacle_position_x()
         self.rect.bottom = random_obstacle_position_y()
@@ -73,8 +38,8 @@ class Test6(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left = 10
         self.rect.top = 10
-        self.speedx = randint(1, 5)
-        self.speedy = randint(1, 5)
+        self.speedx = random.uniform(1.5, 5.5)
+        self.speedy = random.uniform(1.5, 5.5)
     
     def update(self) -> None:
         self.rect.move_ip(self.speedx, self.speedy)
@@ -93,9 +58,14 @@ class Game:
         pygame.display.set_caption("Bitmaps laden und ausgeben")
         self.clock = pygame.time.Clock()
 
-        self.all_mobs = pygame.sprite.Group()
-        self.all_mobs.add(Test1(), Test2(), Test3(), Test4(), Test5())
-        self.all_mobs.add(Test6())
+        self.all_obstacles = pygame.sprite.Group()
+        for _ in range(Settings.OBSTACLE_NUM):
+            self.obstacles = Obstacle("test.png", random.uniform(0.75, 1.5), random.uniform(0.75, 1.5))
+            self.all_obstacles.add(self.obstacles)
+
+        self.object = Test6()
+        self.all_objects = pygame.sprite.Group()
+        self.all_objects.add(self.object)
 
         self.running = True
 
@@ -108,17 +78,20 @@ class Game:
             self.clock.tick(Settings.FPS)
 
     def update(self) -> None:
-        self.all_mobs.update()
+        self.all_objects.update()
 
     def spawn_new_mob(self) -> None:
+        # Wie man Objekte nach einer gewissen Zeit spawnt: https://stackoverflow.com/questions/52917306/i-have-been-trying-to-have-a-new-enemy-spawn-every-20-seconds-using-pygame
         time_now = pygame.time.get_ticks()
         if time_now - self.time_start > Settings.OBJECT_SPAWNRATE:
-            self.time_start = time_now
-            self.all_mobs.add(Test6())        
+            self.time_start = time_now            
+            self.object = Test6()
+            self.all_objects.add(self.object)
 
     def draw(self) -> None:
         self.screen.fill("black")
-        self.all_mobs.draw(self.screen)
+        self.all_obstacles.draw(self.screen)
+        self.all_objects.draw(self.screen)
         pygame.display.flip()
 
     def watch_for_events(self) -> None:
